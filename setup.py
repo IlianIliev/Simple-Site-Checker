2a8fc30d2214d96520de6e0f00bbb5a2c4c6b70f
eng. Ilian Iliev <ilian@i-n-i.org>
2013-09-23 22:06:17 +0200

    Adding setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='SimpleSiteChecker',
+    description='Simple Site Checker is a command line tool that allows you to run a website pages check using XML sitemap',
+    version='0.1.02',
+    author='Ilian Iliev',
+    author_email='ilian@i-n-i.org',
+    scripts=['simple_site_checker.py'],
+    url='http://pypi.python.org/pypi/SimpleSiteChecker/',
+    license='LICENSE',
+    long_description=open('./README.rst').read(),
+    install_requires=[
+        'argparse',
+        'lxml',
+    ],
+)
